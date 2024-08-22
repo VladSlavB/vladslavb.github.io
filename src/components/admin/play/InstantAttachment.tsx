d8a94f171ef5fc66c4df8495005b88b7df5e3fd9
Daniil Reutsky <cromtus@yandex.ru>
2024-08-22 14:34:17 +0200

    instant attachment

--- src/components/admin/play/InstantAttachment.tsx
+++ src/components/admin/play/InstantAttachment.tsx
@@ -1,32 +1,18 @@
-import React, { useState } from 'react'
-import ButtonGroup from '@mui/joy/ButtonGroup'
-import { useDispatch } from '../../../store'
-import Button from '@mui/joy/Button'
-import ImageOutlined from '@mui/icons-material/ImageOutlined'
-import TextFields from '@mui/icons-material/TextFields'
-import styles from './styles.css'
+import React from 'react'
+import { deleteAttachment, showAttachment, useDispatch, useSelector } from '../../../store'
+import AddAttachment from '../edit/AddAttachment'
+import IconButton from '@mui/joy/IconButton'
+import Close from '@mui/icons-material/Close'
 
 
 const InstantAttachment: React.FC = () => {
-  // TODO
-  return null
-  // const [ modalType, setModalType ] = useState<'img' | 'text' | undefined>(undefined)
-  // const dispatch = useDispatch()
-  // return <>
-  //   <ButtonGroup className={styles.outlined}>
-  //     <Button onClick={() => setModalType('img')}><ImageOutlined /></Button>
-  //     <Button onClick={() => setModalType('text')}><TextFields /></Button>
-  //   </ButtonGroup>
-  //   <Modal
-  //     open={modalType != null} onClose={() => {
-  //       setModalType(undefined)
-  //       dispatch(deleteInstantAttachment())
-  //     }}
-  //     type={modalType}
-  //     showMode
-  //     onDone={attachment => dispatch(toggleInstantAttachment(attachment))}
-  //   />
-  // </>
+  const shown = useSelector(state => state.visibility.attachment != null)
+  const dispatch = useDispatch()
+  return shown ? (
+    <IconButton onClick={() => dispatch(deleteAttachment())}><Close /></IconButton>
+  ) : (
+    <AddAttachment onDone={attachment => dispatch(showAttachment(attachment))} />
+  )
 }
 
 export default InstantAttachment
